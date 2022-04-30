f919f3460e6527b5a128a3cc83be88be6608ca7e
tdorobantu <tdorobantu95@gmail.com>
2022-04-30 22:11:37 +0300

    ForgotPass almost done 🙌

--- src/api/index.js
+++ src/api/index.js
@@ -5,3 +5,4 @@ const hostname = process.env.REACT_APP_SERVER_URL
 export const createCampaign = data => axios.post(`${hostname}/api/campaign`, data)
 export const register = data => axios.post(`${hostname}/api/user/register`, data)
 export const login = data => axios.post(`${hostname}/api/user/login`, data);
+export const forgotPass = data => axios.post(`${hostname}/api/user/forgotPass`, data)
--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,28 +1,80 @@
 import React, { useState } from "react";
 import "./Start.css";
+import isEmpty from "lodash/isEmpty";
+import isEmail from "validator/lib/isEmail";
+import * as api from "../api";
+import normalizeEmail from "validator/lib/normalizeEmail";
 
 const ForgotPassword = ({ setPage }) => {
   const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState({ email: "", server: "" });
+  const [validations, setValidations] = useState({ email: false });
 
   const handleEmailInput = (event) => {
-    setEmail((prev) => {
-      return { ...prev, email: event.target.value };
-    });
+    setEmail((prev) => event.target.value);
   };
 
   const handleClick = (page) => {
     setPage((prev) => page);
   };
 
-  const handleSubmit = () => {
-    console.log("credentials: ", email);
+  const handleSubmit = async () => {
+    const blacklistEmail = /[` !#$%^&*()_+\-=[\]{};':"\\|,<>/?~]/g;
+
+    // reset message and validations states
+    setMessage((prev) => {
+      return { email: "", server: "" };
+    });
+
+    setValidations((prev) => {
+      return {
+        email: false,
+      };
+    });
+
+    //check if email input is valid
+    if (!isEmail(email)) {
+      setMessage((prev) => {
+        return {
+          ...prev,
+          email: "Invalid email provided!",
+        };
+      });
+      setValidations((prev) => {
+        return { ...prev, email: false };
+      });
+    } else if (isEmpty(email)) {
+      setMessage((prev) => {
+        return { ...prev, email: "Email field is empty!" };
+      });
+      setValidations((prev) => {
+        return { ...prev, email: false };
+      });
+    } else {
+      setValidations((prev) => {
+        return { ...prev, email: true };
+      });
+    }
+
+    try {
+      const response = await api.forgotPass({
+        email: normalizeEmail(email).replace(blacklistEmail, ""),
+      });
+      setMessage((prev) => {
+        return { ...prev, server: response.data.message };
+      });
+    } catch (error) {
+      setMessage((prev) => {
+        return { ...prev, server: error.response.data.message };
+      });
+    }
   };
 
   return (
     <div className="forgot__container">
       <h1 className="forgot__header">Forgot Password Page</h1>
-      <p>{message}</p>
+      <p className="forgot__message">{message.email}</p>
+      <p className="forgot__message">{message.server}</p>
       <input
         onChange={(event) => handleEmailInput(event)}
         className="forgot__input forgot__input--email"
@@ -32,7 +84,10 @@ const ForgotPassword = ({ setPage }) => {
       <button onClick={handleSubmit} className="forgot__submitBtn">
         Reset Password
       </button>
-      <button className="forgot__switchBtn" onClick={() => handleClick("login")}>
+      <button
+        className="forgot__switchBtn"
+        onClick={() => handleClick("login")}
+      >
         Back to Login Page
       </button>
     </div>
