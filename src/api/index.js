401fd796df2c396851721cb88ca402cd723ee88e
tdorobantu <tdorobantu95@gmail.com>
2022-07-07 14:51:56 +0300

    resendConfirmation added

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
-const hostname = process.env.REACT_APP_SERVER_URL
+const hostname = process.env.REACT_APP_SERVER_URL;
 
-export const createCampaign = data => axios.post(`${hostname}/api/campaign`, data)
-export const register = data => axios.post(`${hostname}/api/user/register`, data)
-export const login = data => axios.post(`${hostname}/api/user/login`, data);
-export const forgotPass = data => axios.post(`${hostname}/api/user/forgotPass`, data)
-export const confirmEmail = data => axios.post(`${hostname}/api/user/confirmEmail`, data) 
+export const createCampaign = (data) =>
+  axios.post(`${hostname}/api/campaign`, data);
+export const register = (data) =>
+  axios.post(`${hostname}/api/user/register`, data);
+export const login = (data) => axios.post(`${hostname}/api/user/login`, data);
+export const forgotPass = (data) =>
+  axios.post(`${hostname}/api/user/forgotPass`, data);
+export const confirmEmail = (data) =>
+  axios.post(`${hostname}/api/user/confirmEmail`, data);
+export const resendConfirmation = (data) =>
+  axios.post(`${hostname}/api/user/resendConfirmation`, data);
